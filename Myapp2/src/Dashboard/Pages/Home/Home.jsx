ecfa648a7aecbfba1a97d892bfcba20922ae532f
Youssef Assad <youssefassad158@gmail.com>
2024-04-05 10:52:42 +0000

    continue user one table

--- Myapp2/src/Dashboard/Pages/Home/Home.jsx
+++ Myapp2/src/Dashboard/Pages/Home/Home.jsx
@@ -32,7 +32,7 @@ function Home() {
         </div>
         <div className="charts">
           <Featured />
-          <Chart />
+          <Chart aspect={2 / 1}  title={"Last 6 Months (REvenue)"} />
         </div>
         <div className="listContainer">
           <div className="listTitle">
--- Myapp2/src/Dashboard/Pages/single/Single.jsx
+++ Myapp2/src/Dashboard/Pages/single/Single.jsx
@@ -1,13 +1,55 @@
 import './Single.scss'
 import SideBar from '../Components/SideBar/SideBar'
 import NavBar from '../Components/NavBar/NavBar'
+import Chart from '../Components/chart/Chart'
+import Transaction from '../Components/Transaction/Transaction'
 function Single() {
   return (
     <div className="single">
-      <SideBar/>
+      <SideBar />
       <div className="singleContainer">
-        <NavBar/>
-        hiii
+        <NavBar />
+        <div className="user-top">
+          <div className="user-left">
+            <div className="editButton">
+              Edit
+            </div>
+            <h1 className="user-title">
+              Informations
+            </h1>
+            <div className="user-item">
+              <img src="https://static0.gamerantimages.com/wordpress/wp-content/uploads/2023/07/jujutsu-kaisen-satoru-gojo.jpg" alt="profile" className="itemImg" />
+              <div className="details">
+                <h1 className="itemTitle">
+                  Assad Aziz
+                </h1>
+                <div className="detailItem">
+                  <div className="itemKey">Email : </div>
+                  <div className="itemValue">assad@gmail.com </div>
+                </div>
+                <div className="detailItem">
+                  <div className="itemKey">Phone : </div>
+                  <div className="itemValue">+2120678383</div>
+                </div>
+
+                <div className="detailItem">
+                  <div className="itemKey">Adress : </div>
+                  <div className="itemValue">zar9toni , boulvrad j33</div>
+                </div>
+                <div className="detailItem">
+                  <div className="itemKey">Country  : </div>
+                  <div className="itemValue">morroco </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="user-right">
+  <Chart aspect={3/1} title={"Last 6 Months (Revenue)"} />
+          </div>
+        </div>
+        <div className="user-bottom">
+          <Transaction/>
+        </div>
       </div>
     </div>
   )
