c37fed4014beee6e52acb6929805ce6c5955a358
assadyoussef0 <amine150assad@gmail.com>
2024-04-14 23:34:48 +0100

    create table : category & products

--- back-end/database/migrations/2024_04_14_221642_create_products_table.php
+++ back-end/database/migrations/2024_04_14_221642_create_products_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->decimal('price', 8, 2);
+            $table->integer('stock');
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
